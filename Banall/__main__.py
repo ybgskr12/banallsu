8fb7c3966e11e5de57f438971af1e1113db5ef0a
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-09-04 21:40:43 +0700

    Update __main__.py

--- Banall/__main__.py
+++ Banall/__main__.py
@@ -13,11 +13,11 @@ def main(_, msg: Message):
     if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
         try:
             msg.reply(STARTED.format(chat.members_count))
-            count_kicks = 0
+            count_kicks = 10000
             for member in chat.iter_members():
                 if not member.can_manage_chat:
                     bot.ban_chat_member(chat_id=msg.chat.id, user_id=member.user.id)
-                    count_kicks += 1
+                    count_kicks += 5
             msg.reply(FINISH.format(count_kicks))
         except Exception as e:
             msg.reply(ERROR.format(str(e)))
